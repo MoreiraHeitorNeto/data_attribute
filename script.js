7e4af825a5359feefc4115caa07605fe151df652
User <heitor.moreira.neto@outlook.com>
2023-01-09 11:42:36 -0300

    desenvolvendo o algoritmo

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert('o js foi implementado');
+const listaCores = document.querySelectorAll('.item');
+
+document.querySelector('#deleta').addEventListener('click', deletaItens);
+document.querySelector('#reseta').addEventListener('click', resetaItens);
+
+function deletaItens(){
+    listaCores.forEach(element => {
+        element.innerHTML='';
+    })
+}
+
+function resetaItens(){
+    window.location.reload();
+}
